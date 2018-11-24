35842f00f00a7c1770b22b6baa9c40442fe1e1d7
Karthick <karthickmadhiazhagan@gmail.com>
2018-11-24 17:10:36 +0530

    Refactor update quality using factory pattern

--- gilded_rose/lib/gilded_rose.rb
+++ gilded_rose/lib/gilded_rose.rb
@@ -1,51 +1,101 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-    end
+	updateter = ItemUpdaterFactory.getUpdater(item.name)
+	item = updateter.updateItem(item)
   end
 end
 
+class ItemUpdaterFactory
+	def self.getUpdater(name)		
+		case name
+		when "Aged Brie"
+			AgedBrieUpdater.new
+		when "Backstage Passes"
+			BackstagePassesUpdater.new
+		when "Sulfuras"
+			SulfurasUpdater.new
+		else
+			NormalUpdater.new
+		end
+	end
+end
+
+class  NormalUpdater
+	def updateItem(item)
+		updated_item = update_sell_in(item)
+		updated_item = update_quality(item)
+		updated_item
+	end
+	
+	def update_sell_in(item)
+		item.sell_in += get_sell_in_loss
+	end
+	
+	def update_quality(item)
+		item.quality += (get_quality_loss(item) * get_quality_loss_multiplier(item)) if item.quality > 0 
+		normalize_item(item)
+	end
+	
+	def normalize_item(item)
+		item.quality = 0 if item.quality < 0 
+		item.quality = getQualityMax if item.quality > getQualityMax
+		item
+	end
+	
+	# def get_modified_quality(current_quality)
+		# current_quality + get_quality_loss > getQualityMax
+	# end
+	
+	def get_sell_in_loss
+		-1
+	end
+	
+	def get_quality_loss(item=nil)
+		-1
+	end
+	
+	def get_quality_loss_multiplier(item)
+		isExpired(item) ? 2 : 1
+	end
+	
+	def isExpired(item)
+		item.sell_in < 0
+	end
+	
+	# def isPerfectQuality(item)
+		# item.quality >= getQualityMax
+	# end
+	
+	def getQualityMax
+		50
+	end
+end
+
+class  AgedBrieUpdater < NormalUpdater
+	
+	def get_quality_loss(item=nil)
+		1
+	end
+end
+
+class  BackstagePassesUpdater < AgedBrieUpdater
+	
+	def get_quality_loss(item=nil)
+		quality_loss = super.get_quality_loss
+		quality_loss += 1 if item.quality <= 10
+		quality_loss += 1 if item.quality <= 5
+		quality_loss = (- item.quality) if item.sell_in < 0
+	end
+	
+	
+end
+
+class  SulfurasUpdater < NormalUpdater
+	
+	def updateItem(item)
+		item
+	end
+end
 ######### DO NOT CHANGE BELOW #########
 
 Item = Struct.new(:name, :sell_in, :quality)
