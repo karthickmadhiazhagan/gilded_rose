f213e5dbadb98d49a7e0b06efbafeb6cd7ff87b4
Karthick <karthickmadhiazhagan@gmail.com>
2018-11-27 01:21:47 +0530

    Test cases for single items

--- gilded_rose/spec/gilded_rose_spec.rb
+++ gilded_rose/spec/gilded_rose_spec.rb
@@ -4,18 +4,118 @@ describe "#update_quality" do
 
   context "with a single item" do
     let(:initial_sell_in) { 5 }
-    let(:initial_quality) { 50 }
+    let(:initial_quality) { 48 }
     let(:name) { "Noramal Item" }
     let(:item) { Item.new(name, initial_sell_in, initial_quality) }
 
     before { update_quality([item]) }
-	context "when normal item" do
+	
+	shared_examples "normal item" do
+		it "quality should not be negative" do
+		  expect(item.quality).to be > 0 
+		end
+		
 		it "quantity should not be greater than 50" do
 		  expect(item.quality).to be <= 50
 		end
+	end
+	
+	shared_examples "aged brie" do
+		it "quantity should increase by 1" do
+		  expect(item.quality).to eq(49)
+		end
 		
-		it "quantity should not be negative" do
-		  expect(item.quality).to be > 0 
+		context "reached the max quality" do
+			let(:initial_sell_in) {4}
+			let(:initial_quality) {50}
+			it "then quantity does not get increase" do
+				expect(item.quality).to eq(50)
+			end
+		end
+	end
+	
+	context "when normal item" do		
+		
+		it_behaves_like 'normal item'		
+		
+		it "quantity should degrades by 1" do
+		  expect(item.quality).to eq(47)
+		end
+		
+		context "when sell in date passed" do
+			let(:initial_sell_in) {0}
+			let(:initial_quality) {48}
+			it "quantity should degrades by 2" do
+				expect(item.quality).to eq(46)
+			end
+		end
+	end
+	
+	context "when aged brie item" do		
+		let(:name) { "Aged Brie" }		
+		let(:initial_quality) { 48 }
+		
+		it_behaves_like 'normal item'
+		it_behaves_like 'aged brie'
+		
+	end
+	
+	context "when backstage passes item" do		
+		let(:name) { "Backstage Passes" }
+		let(:initial_sell_in) {13}
+		let(:initial_quality) { 48 }
+		
+		it_behaves_like 'normal item'
+		it_behaves_like 'aged brie'
+		
+		context "sell in less than equal to 10" do
+			let(:initial_sell_in) {10}
+			let(:initial_quality) {45}
+			it "then quantity should increase by 2" do
+				expect(item.quality).to eq(47)
+			end
+		end
+		
+		context "sell in less than equal to 5" do
+			let(:initial_sell_in) {5}
+			let(:initial_quality) {45}
+			it "then quantity should increase by 3" do
+				expect(item.quality).to eq(48)
+			end
+		end
+		
+		context "sell in less than equal to 0" do
+			let(:initial_sell_in) {0}
+			let(:initial_quality) {45}
+			it "then quantity should drops to 0" do
+				expect(item.quality).to eq(0)
+			end
+		end
+	end
+	
+	context "when conjured" do		
+		let(:name) { "Conjured" }	
+		it_behaves_like 'normal item'		
+		
+		it "quantity should degrades by 2" do
+		  expect(item.quality).to eq(46)
+		end
+		
+		context "when sell in date passed" do
+			let(:initial_sell_in) {0}
+			let(:initial_quality) {48}
+			it "quantity should degrades by 4" do
+				expect(item.quality).to eq(44)
+			end
+		end
+	end
+	
+	
+	context "when Sulfuras item" do
+		let(:name) { "Sulfuras" }
+		let(:initial_quality) {80}
+		it "quantity should be 80" do
+		  expect(item.quality).to eq(80)
 		end
 	end
   end
@@ -97,18 +197,6 @@ describe "#update_quality" do
 			expect(items[index].sell_in).to eq(ans[0])
 			expect(items[index].quality).to eq(ans[1])
 		end
-		# expect(items[0].sell_in).to eq(4)
-		# expect(items[0].quality).to eq(9)
-		# expect(items[1].sell_in).to eq(-1)
-		# expect(items[1].quality).to eq(13)
-		# expect(items[2].sell_in).to eq(-2)
-		# expect(items[2].quality).to eq(18)
-		# expect(items[3].sell_in).to eq(4)
-		# expect(items[3].quality).to eq(0)
-		# expect(items[4].sell_in).to eq(-1)
-		# expect(items[4].quality).to eq(0)
-		# expect(items[5].sell_in).to eq(-2)
-		# expect(items[5].quality).to eq(0)
 	end
   end
 end
